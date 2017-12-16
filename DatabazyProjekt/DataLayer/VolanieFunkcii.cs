0ca1b1c702e1aab093d25871200dfe20a15aae6e
marek481 <pistekm@azet.sk>
2017-12-16 15:45:11 +0100

    daj aktualnu polohu vozna

--- DatabazyProjekt/DataLayer/VolanieFunkcii.cs
+++ DatabazyProjekt/DataLayer/VolanieFunkcii.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Types;
 
 namespace DataLayer
 {
@@ -69,6 +70,31 @@ namespace DataLayer
             
         }
 
+	    public void DajPolohuVozna(int idVozna, out double zemDlzka, out double zemSirka)
+	    {
+			var cmd = new OracleCommand
+			{
+				Connection = _connection,
+				CommandText = "daj_polohu_vozna",
+				CommandType = CommandType.StoredProcedure,
+				BindByName = true
+			};
 
+		    cmd.Parameters.Add("pa_id_vozna", OracleDbType.Int32, ParameterDirection.Input).Value = idVozna;
+		    cmd.Parameters.Add("pa_zem_sirka", OracleDbType.Decimal, ParameterDirection.Output);
+		    cmd.Parameters.Add("pa_zem_dlzka", OracleDbType.Decimal, ParameterDirection.Output);
+
+		    try
+		    {
+				_connection.Open();   							    
+			    cmd.ExecuteNonQuery();
+			    zemDlzka = (double) (OracleDecimal) cmd.Parameters["pa_zem_dlzka"].Value;
+			    zemSirka = (double) (OracleDecimal) cmd.Parameters["pa_zem_sirka"].Value;				
+		    }
+		    finally
+		    {
+			    _connection.Close();
+		    }
+		}		    
     }
 }
